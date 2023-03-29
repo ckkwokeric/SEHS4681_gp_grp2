059c97b949633bf5116c753465b0bc2b134ce7e5
Eric Kwok <ckkwokeric112@gmail.com>
2023-03-29 15:08:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sehs4681_gp_grp2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sehs4681_gp_grp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
